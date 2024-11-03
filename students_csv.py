dec20442172aa6b8724f0ee70a47c100f062a799
slawomiras <slawcio20@vp.pl>
2024-11-03 14:35:41 +0100

    Revision of code

--- students_csv.py
+++ students_csv.py
@@ -3,7 +3,11 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        students.append(f"{name} is in {house}")
+        student = {"name": name, "house": house}
+        students.append(student)
 
-for student in sorted(students):
-    print(student)
+def get_name(student):
+    return student["name"]
+
+for student in sorted(students, key=get_name):
+    print(f"{student['name']} is in {student['house']}")
