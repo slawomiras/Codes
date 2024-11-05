05d10e65f817485abd7622e6f2006405e7207c85
slawomiras <slawcio20@vp.pl>
2024-11-05 14:41:11 +0100

    update

--- students_lib_csv.py
+++ students_lib_csv.py
@@ -2,10 +2,10 @@ import csv
 
 students = []
 
-with open("students_home.csv") as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home": home})
+with open("students_write.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
 
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is from {student['home']}")
--- students_write_to_csv.py
+++ students_write_to_csv.py
@@ -4,5 +4,5 @@ name = input("What's your name? ")
 home = input("Where's your home? ")
 
 with open("students_write.csv", "a") as file:
-    writer = csv.writer(file)
-    writer.writerow([name, home])
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
