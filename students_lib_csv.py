b9fcdbeb1a832accca2824ff18d853f748a9569d
slawomiras <slawcio20@vp.pl>
2024-11-05 10:19:57 +0100

    csv reader

--- students_lib_csv.py
+++ students_lib_csv.py
@@ -2,11 +2,10 @@ import csv
 
 students = []
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+with open("students_home.csv") as file:
+    reader = csv.reader(file)
+    for name, home in reader:
+        students.append({"name": name, "home": home})
 
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is from {student['home']}")
