a15b9a61404fc308434a2e8967ef4f342afa6078
slawomiras <slawcio20@vp.pl>
2024-11-10 18:01:42 +0100

    format improvements

---
+++ format.py
@@ -0,0 +1,12 @@
+import re
+
+name = input("What's your name? ").strip()
+
+matches = re.search(r"^(.+), (.+)$", name)
+
+if matches:
+    last = matches.group(1)
+    first = matches.group(2)
+    name = f"{first} {last}"
+
+print(f"hello, {name}")
