69204b7beaa70ae95730302845b07b44f40413a1
slawomiras <slawcio20@vp.pl>
2024-11-13 16:00:36 +0100

    Changes

--- twitter.py
+++ twitter.py
@@ -2,5 +2,5 @@ import re
 
 url = input("URL: ").strip()
 
-if matches := re.search(r"^(?:https?)?://(?:www\.)?x\.com/(.+)$", url, re.IGNORECASE):
+if matches := re.search(r"^(?:https?)?://(?:www\.)?x\.com/([a-z0-9_]+)", url, re.IGNORECASE):
     print(f"Username:", matches.group(1))
