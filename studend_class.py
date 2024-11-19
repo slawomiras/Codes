4bb9ac0d2d73c50c397a2e3878d618ac0082b9ca
slawomiras <slawcio20@vp.pl>
2024-11-19 16:25:25 +0100

    copy of Student with "class" function

---
+++ studend_class.py
@@ -0,0 +1,16 @@
+class Student:
+    ...
+    
+
+def main():
+    student = get_student()
+    print(f"{student['name']} from {student['name']}")
+
+def get_student():
+    student = {}
+    student["name"] = input("Name: ")
+    student["house"] = input("House: ")
+    return student
+
+if __name__ == "__main__":
+    main()
