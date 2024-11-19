0853f3780c862bba71843f75aea8743caaa5616c
slawomiras <slawcio20@vp.pl>
2024-11-19 16:50:13 +0100

    Nowy kod do classesss

--- student_class.py
+++ student_class.py
@@ -3,12 +3,13 @@ class Student:
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.name}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
     student = Student()
     student.name = input("Name: ")
     student.house = input("House: ")
+    return student
 
 
 if __name__ == "__main__":
