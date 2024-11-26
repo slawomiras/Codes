3d6b801d814418289845472cd6e863471832dfe3
slawomiras DELL <slawcio20@vp.pl>
2024-11-26 20:56:41 +0100

    ggiAdded classess function

--- student_class.py
+++ student_class.py
@@ -1,15 +1,20 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
 
 def main():
     student = get_student()
     print(f"{student.name} from {student.house}")
 
 def get_student():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
 
 
 if __name__ == "__main__":
