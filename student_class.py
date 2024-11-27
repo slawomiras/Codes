2853453ffb3f0d4f540fa2fb746be5c53a3a59d9
slawomiras DELL <slawcio20@vp.pl>
2024-11-27 09:44:48 +0100

    New method

--- student_class.py
+++ student_class.py
@@ -1,24 +1,37 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
         
     def __str__(self):
         return f"{self.name} is from {self.house}"
+    
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🐴"
+            case "Otter":
+                return "🦦"
+            case "Terrier":
+                return "🐶"
+            case _:
+                return "🥢"
 
 def main():
     student = get_student()
-    print(student)
+    print("Expecto patronum!")
+    print(student.charm())
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
-
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 if __name__ == "__main__":
     main()
