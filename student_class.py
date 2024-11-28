b2350a242f5fccc6748922a5aa9ab74a9da4bb16
slawomiras DELL <slawcio20@vp.pl>
2024-11-28 07:27:14 +0100

    Deleted patronus

--- student_class.py
+++ student_class.py
@@ -1,28 +1,23 @@
 class Student:
-    def __init__(self, name, house, patronus):
+    def __init__(self, name, house):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
-        self.patronus = patronus
         
     def __str__(self):
         return f"{self.name} is from {self.house}"
     
-
-
 def main():
     student = get_student()
-    print("Expecto patronum!")
-    print(student.charm())
+    print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    patronus = input("Patronus: ")
-    return Student(name, house, patronus)
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
