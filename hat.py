96db2719ec14401395e7501a73a43afc2ae7e05a
slawomiras DELL <slawcio20@vp.pl>
2024-12-05 08:20:18 +0100

    New classmethod

--- hat.py
+++ hat.py
@@ -1,11 +1,11 @@
 import random
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
-        
-    def sort(self, name):
-        print(name, "is in", random.choice(self.houses))
     
-hat = Hat()
-hat.sort("Harry")
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+        
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
+    
+Hat.sort("Harry")
