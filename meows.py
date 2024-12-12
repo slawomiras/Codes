61e82bc54026a736f40d6c713ac8f64e3b33d8e7
slawomiras DELL <slawcio20@vp.pl>
2024-12-12 09:55:16 +0100

    changes, changes

--- meows.py
+++ meows.py
@@ -1,7 +1,8 @@
-def meow(n: int) -> str:
-    return "meow\n" * n
-        
+import argparse
 
-number: int = int(input("Number: "))
-meows: str = meow(number)
-print(meows, end="")
+parser = argparse.ArgumentParser(description="Meow like a cat")
+parser.add_argument("-n", default=1, help="number of times to meow", type=int)
+args = parser.parse_args()
+
+for _ in range(int(args.n)):
+    print("meow")
