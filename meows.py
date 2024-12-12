2021f8852577183276cdacc683c79a5be21f1b89
slawomiras DELL <slawcio20@vp.pl>
2024-12-12 07:54:16 +0100

    changes

--- meows.py
+++ meows.py
@@ -1,7 +1,7 @@
-def meow(n: int):
-    for _ in range(n):
-        print("meow")
+def meow(n: int) -> str:
+    return "meow\n" * n
         
 
-number = input("Number: ")
-meow(number)
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows, end="")
