bf8c17c3dbd1ae2224b3ab6e4fb29c82ee1a8980
slawomiras DELL <slawcio20@vp.pl>
2024-12-18 09:26:57 +0100

    disctionary comprehension

--- gryffindors.py
+++ gryffindors.py
@@ -1,15 +1,5 @@
-students = [
-    {"name": "Hermione", "house": "Gryffindor"},
-    {"name": "Harry", "house": "Gryffindor"},
-    {"name": "Ron", "house": "Gryffindor"},
-    {"name": "Draco", "house": "Slytherin"},
-]
+students = ["Hermione", "Harry", "Ron"]
 
-def is_gryffindor(s):
-    return s["house"] == "Gryffindor"
-
-
-gryffindors = filter(is_gryffindor, students)
-
-for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
-    print(gryffindor["name"])
+gryffindors = {student: "Gryffindor" for student in students}
+    
+print(gryffindors)
